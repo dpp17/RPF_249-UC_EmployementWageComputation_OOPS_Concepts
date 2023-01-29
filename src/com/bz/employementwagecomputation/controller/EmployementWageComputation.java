e5fd422830cb9cec4fb4b6d0010a003ae3aeeddc
dpp17 <durga123q.com>
2023-01-29 16:28:06 +0530

    Added: EmployementWageComputation fullTime employee wage added

--- src/com/bz/employementwagecomputation/controller/EmployementWageComputation.java
+++ src/com/bz/employementwagecomputation/controller/EmployementWageComputation.java
@@ -1,18 +1,36 @@
 package com.bz.employementwagecomputation.controller;
 
 import com.bz.employementwagecomputation.interfaces.IWageComputation;
+import com.bz.employementwagecomputation.model.Computation;
 import com.bz.employementwagecomputation.services.ImplementationWageComputation;
 
 public class EmployementWageComputation {
 
+	static Computation computation = new Computation();
 	public static void main(String[] args) {
 		
 		IWageComputation getEmployee = new ImplementationWageComputation();
-//		superclass name = new subclass();
 			
+//displaying welcome message
 		getEmployee.displayMessage();
-		System.out.println("Fill_Time Employees Present :: " + getEmployee.employeesPresentFullTime());
+		
+//number of employees present per day
+		computation.setPresent(getEmployee.employeesPresentFullTime());
+		System.out.println("Fill_Time Employees Present :: " + computation.getPresent() + '\n');
+
+//wage for single employee per day
+		System.out.println("EmployementWage Per Day :: " + getEmployee.dailyFullTimeEmployeeWage(computation.wagePerHour, computation.fullDayHour, 1) + '\n');
+
+		
+		
 		
 	}
 
+	private static Computation getComputation(){
+		
+
+
+		return computation;
+		
+	}
 }
