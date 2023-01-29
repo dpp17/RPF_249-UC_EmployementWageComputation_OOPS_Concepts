1af1eea418b603bf93694990c167bb63b03c6943
dpp17 <durga123q.com>
2023-01-29 21:56:02 +0530

    Added: Employement Wage For A Month

--- src/com/bz/employementwagecomputation/services/ImplementationWageComputation.java
+++ src/com/bz/employementwagecomputation/services/ImplementationWageComputation.java
@@ -8,34 +8,71 @@ public class ImplementationWageComputation implements IWageComputation {
 		System.out.println(" :: Welcome To Employement Wage Computation :: ");
 		
 	}
+	
+	
+	private int employeesPresent() {
+		int present = (int)(Math.random()*100+20);
+		return present;
+	}
+	private int employeesWagePerDay(int wagePerHour, int TimeHour, int numberOfEmployees) {
+		int wagePerDay = TimeHour * wagePerHour * numberOfEmployees;
+		return wagePerDay;
+	}
+	
+	
 
 	public int employeesPresentFullTime() {
-		int present = (int)(Math.random()*100+1);
+		int present = employeesPresent();
 		System.out.println("Full_Time Employees Present :: " + present + '\n');
 		return present;
 	}
-
 	public int dailyFullTimeEmployeeWage(int wagePerHour, int fullDayHour, int numberOfEmployees) {
-		int wagePerDay = fullDayHour * wagePerHour * numberOfEmployees;
-		System.out.println("EmployementWage for a Full_Timer Per Day :: " + wagePerDay + '\n');
+		int wagePerDay = employeesWagePerDay(wagePerHour, fullDayHour, numberOfEmployees);
+		System.out.println("EmployementWage for a Full_Timer For Today :: " + wagePerDay + '\n');
 		return wagePerDay;
 	}
 
+	
+	
 	public int employeesPresentPartTime() {
-		int present = (int)(Math.random()*10+20);
+		int present = employeesPresent()/3;
 		System.out.println("Part_Time Employees Present :: " + present + '\n');
 		return present;
 	}
-
 	public int dailyPartTimeEmployeeWage(int wagePerHour, int PartTimeHour, int numberOfEmployees) {
-		int wagePerDay = PartTimeHour * wagePerHour * numberOfEmployees;
+		int wagePerDay = employeesWagePerDay(wagePerHour, PartTimeHour, numberOfEmployees);
 		System.out.println("EmployementWage for a Part_Timer Per Day :: " + wagePerDay + '\n');
 		return wagePerDay;
 	}
 
+	
+	
+	public int wageForMonth(int workingDays, int wagePerHour, int TimeHour) {
+		int totalWageForMonth = 0;
+		for(int i = 1; i <=workingDays ; i++) {
+			int numberOfEmployees = employeesPresent();
+			System.out.println("Wage on Day{ "+i+" } :: " + employeesWagePerDay(wagePerHour, TimeHour, numberOfEmployees));
+			totalWageForMonth = totalWageForMonth + employeesWagePerDay(wagePerHour, TimeHour, numberOfEmployees);
+		}	
+		System.out.println("Total Wage Given To All The Employees for the Month :: " + totalWageForMonth + '\n');
+		return totalWageForMonth;
+	}
+
+
+
 
 	
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 
