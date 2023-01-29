0f8de38c135a9e305131ff4ce2c687f246d21bff
dpp17 <durga123q.com>
2023-01-29 08:35:45 +0530

    Added: Employement Wage Implementations of abstract methods of Interface class

---
+++ src/com/bz/employementwagecomputation/services/ImplementationWageComputation.java
@@ -0,0 +1,15 @@
+package com.bz.employementwagecomputation.services;
+
+import com.bz.employementwagecomputation.interfaces.IWageComputation;
+
+public class ImplementationWageComputation implements IWageComputation {
+
+	public void displayMessage() {
+		System.out.println(" :: Welcome To Employement Wage Computation :: ");
+		
+	}
+
+	
+	
+
+}
