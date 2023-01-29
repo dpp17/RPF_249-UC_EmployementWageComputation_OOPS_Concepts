6eee91cdae14e04448b2642ccfbc334b8d3290cf
dpp17 <durga123q.com>
2023-01-29 21:54:29 +0530

    Added: Employement Wage For A Month

--- src/com/bz/employementwagecomputation/controller/EmployementWageComputation.java
+++ src/com/bz/employementwagecomputation/controller/EmployementWageComputation.java
@@ -18,7 +18,7 @@ public class EmployementWageComputation {
 		getEmployee.displayMessage();
 		
 		do {
-		System.out.println(" 1.FullTime Employement Wages"+'\n'+" 2.PartTime Employement Wages"+'\n'+" Enter option::");
+		System.out.println(" 1.FullTime Employement Wages"+'\n'+" 2.PartTime Employement Wages"+'\n'+" 3.Employement Wage For A Month :: "+'\n'+" Enter option::");
 		option = getUserInput.nextByte();
 		switch(option) {
 		case 1:	
@@ -32,14 +32,23 @@ public class EmployementWageComputation {
 			computation.setPresentPartTime(getEmployee.employeesPresentPartTime());
 //wage for single employee per day
 			System.out.println("Enter number of Working Hour:: ");
-			computation.setPartTimeHour(getUserInput.nextInt());
-			
-			computation.setPartTimeWage(getEmployee.dailyPartTimeEmployeeWage(computation.getPartTimeHour(), computation.wagePerHour, computation.getPresentPartTime()));
+			computation.setPartTimeHour( getUserInput.nextInt());
+			computation.setPartTimeWage( getEmployee.dailyPartTimeEmployeeWage(computation.getPartTimeHour(), computation.wagePerHour, computation.getPresentPartTime()));
 		break;
+//Employement_Wage for a Month
+		case 3:
+			System.out.println("Press 1. For FullTime Employement Wages For A Month"+'\n'+"Press 2. For PartTime Employement Wages For A Month");
+			byte choice = getUserInput.nextByte();
+			if(choice == 1) {
+				computation.setWageForMonth(getEmployee.wageForMonth(20, computation.wagePerHour, computation.fullDayHour));
+			}else if(choice ==2) {
+				computation.setWageForMonth(getEmployee.wageForMonth(20, computation.wagePerHour, computation.fullDayHour));
+			}
+			break;
 		default:
 			System.err.println(" :: INVALID INPUT! try again :: ");
 		}
-		}while(option > 2 || option < 1);
+		}while(option > 3 || option < 1);
 		
 		System.out.println("-->>>  Thank You for Using :: Employement Wage Computation Device ::  <<<--");
 		
