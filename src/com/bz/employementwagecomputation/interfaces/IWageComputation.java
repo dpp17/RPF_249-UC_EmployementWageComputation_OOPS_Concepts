dac4831090b100c4fc4d435e791799c4e8ac60f9
dpp17 <durga123q.com>
2023-01-29 17:44:16 +0530

    Added: EmployementWageComputation partTime employee wage added

--- src/com/bz/employementwagecomputation/interfaces/IWageComputation.java
+++ src/com/bz/employementwagecomputation/interfaces/IWageComputation.java
@@ -6,5 +6,7 @@ public interface IWageComputation {
 	 void displayMessage();
 	 int employeesPresentFullTime();
 	 int dailyFullTimeEmployeeWage(int wagePerHour, int fullDayHour, int numberOfEmployees);
+	 int employeesPresentPartTime();
+	 int dailyPartTimeEmployeeWage(int wagePerHour, int PartTimeHour, int numberOfEmployees);
 
 }
