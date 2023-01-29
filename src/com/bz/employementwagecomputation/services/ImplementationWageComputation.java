ba5e7e12b38e083265f2786c25452d7594b11ec8
dpp17 <durga123q.com>
2023-01-29 19:38:37 +0530

    Added: EmployementWageComputatio Switched Case Option added

--- src/com/bz/employementwagecomputation/services/ImplementationWageComputation.java
+++ src/com/bz/employementwagecomputation/services/ImplementationWageComputation.java
@@ -11,21 +11,25 @@ public class ImplementationWageComputation implements IWageComputation {
 
 	public int employeesPresentFullTime() {
 		int present = (int)(Math.random()*100+1);
+		System.out.println("Full_Time Employees Present :: " + present + '\n');
 		return present;
 	}
 
 	public int dailyFullTimeEmployeeWage(int wagePerHour, int fullDayHour, int numberOfEmployees) {
 		int wagePerDay = fullDayHour * wagePerHour * numberOfEmployees;
+		System.out.println("EmployementWage for a Full_Timer Per Day :: " + wagePerDay + '\n');
 		return wagePerDay;
 	}
 
 	public int employeesPresentPartTime() {
 		int present = (int)(Math.random()*10+20);
+		System.out.println("Part_Time Employees Present :: " + present + '\n');
 		return present;
 	}
 
 	public int dailyPartTimeEmployeeWage(int wagePerHour, int PartTimeHour, int numberOfEmployees) {
 		int wagePerDay = PartTimeHour * wagePerHour * numberOfEmployees;
+		System.out.println("EmployementWage for a Part_Timer Per Day :: " + wagePerDay + '\n');
 		return wagePerDay;
 	}
 
